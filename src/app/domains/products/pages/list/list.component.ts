fcd1ee6b6bd6f8a42231b6fce622eef397a7fe11
eufanzky <eufanzky@gmail.com>
2024-07-02 20:05:48 -0400

    updated product componente

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from '../../components/product/product.component';
+import { Product } from '../../../shared/models/product.model';
 
 @Component({
   selector: 'app-list',
@@ -10,6 +11,35 @@ import { ProductComponent } from '../../components/product/product.component';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent {
+  products = signal<Product[]>([])
+
+  constructor() {
+    const initProducts: Product[] = [
+      {
+        id: 1,
+        title: 'Producto 1',
+        price: '100',
+        image: 'https://picsum.photos/640/640?r=12',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: 2,
+        title: 'Producto 2',
+        price: '200',
+        image: 'https://picsum.photos/640/640?r=23',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: 3,
+        title: 'Producto 3',
+        price: '300',
+        image: 'https://picsum.photos/640/640?r=50',
+        creationAt: new Date().toISOString()
+      }
+    ]
+    this.products.set(initProducts)
+  }
+
   fromChild(event: string) {
     console.log('estamos en el padre');
     console.log(event);
---
+++ src/app/domains/shared/components/counter/counter.component.ts
@@ -0,0 +1,73 @@
+import { Component, Input, SimpleChanges, signal, PLATFORM_ID, Inject } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { repeat } from 'rxjs';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent {
+  @Input({required: true}) duration: number = 0;
+  @Input({required: true}) message: string = '';
+  counter = signal(0);
+  counterRef: number | undefined;
+
+  constructor( @Inject(PLATFORM_ID) private platformId: Object) {
+    //No Async
+    //before render
+    console.log('CounterComponent constructor');
+    console.log('-', repeat(10));
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    //No Async
+    //before and during render
+    console.log('CounterComponent ngOnChanges');
+    console.log('changes', changes);
+    console.log('-', repeat(10));
+    const duration = changes['duration'];
+    if (duration && duration.currentValue !== duration.previousValue) {
+      this.doSomething();
+    }
+  }
+
+  ngOnInit() {
+    //Async
+    //after render
+    //only runs once
+    console.log('CounterComponent ngOnInit');
+    console.log('duration =>', this.duration);
+    console.log('message =>', this.message);
+    console.log('-', repeat(10));
+    if(isPlatformBrowser(this.platformId)){
+      this.counterRef = window.setInterval(() => {
+        console.log('run interval')
+        this.counter.update(statePrev => statePrev + 1);
+      },1000)
+    }
+
+  }
+
+  ngAfterViewInit() {
+    //Async
+    //after render
+    //sons have been rendered
+    console.log('CounterComponent ngAfterViewInit');
+    console.log('-', repeat(10));
+  }
+  ngOnDestroy() {
+    //Async
+    //before render
+    console.log('CounterComponent ngOnDestroy');
+    console.log('-', repeat(10));
+    if(isPlatformBrowser(this.platformId)){
+      window.clearInterval(this.counterRef);
+    }
+  }
+  doSomething() {
+    console.log('change duration');
+  }
+}
