a1b32513877460a9824e3bd6ac6e4cd20a16880b
eufanzky <eufanzky@gmail.com>
2024-07-03 20:58:35 -0400

    added header

--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -2,11 +2,12 @@ import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from '../../components/product/product.component';
 import { Product } from '../../../shared/models/product.model';
+import { HeaderComponent } from '../../../shared/components/header/header.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CommonModule, ProductComponent],
+  imports: [CommonModule, ProductComponent, HeaderComponent],
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
@@ -34,6 +35,27 @@ export class ListComponent {
         title: 'Producto 3',
         price: '300',
         image: 'https://picsum.photos/640/640?r=50',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: 4,
+        title: 'Producto 4',
+        price: '300',
+        image: 'https://picsum.photos/640/640?r=24',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: 5,
+        title: 'Producto 5',
+        price: '300',
+        image: 'https://picsum.photos/640/640?r=64',
+        creationAt: new Date().toISOString()
+      },
+      {
+        id: 6,
+        title: 'Producto 6',
+        price: '300',
+        image: 'https://picsum.photos/640/640?r=12',
         creationAt: new Date().toISOString()
       }
     ]
