e1dad81f16d1a872c772bfd085f257f289f5e90e
eufanzky <eufanzky@gmail.com>
2024-07-05 20:31:58 -0400

    added cart service

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, signal } from '@angular/core';
+import { Component, inject, Input, signal, SimpleChanges } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Product } from '../../models/product.model';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -11,7 +12,9 @@ import { Product } from '../../models/product.model';
 })
 export class HeaderComponent {
   hideSideMenu = signal(true)
-  @Input({required: true}) cart: Product[] = [];
+  private cartService = inject(CartService);
+  cart = this.cartService.cart;
+  total = this.cartService.total;
 
   toggleSideMenu() {
     this.hideSideMenu.update(prevState => !prevState)
